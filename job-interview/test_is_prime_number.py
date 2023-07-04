1b85e0d6f1e7c1afe38090690db922c54012a863
Alex Granovsky <compfaculty@users.noreply.github.com>
2023-07-04 23:58:40 +0300

    pytest-benchmarking added

--- job-interview/test_is_prime_number.py
+++ job-interview/test_is_prime_number.py
@@ -1,7 +1,16 @@
 from pytest_benchmark.plugin import benchmark
 
 
-def is_prime_number(n: int) -> bool:
+def is_prime_numberV1(n: int) -> bool:
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def is_prime_numberV2(n: int) -> bool:
     if n <= 1:
         return False
     if n <= 3:
@@ -16,36 +25,52 @@ def is_prime_number(n: int) -> bool:
     return True
 
 
-# Test is_prime_number()
-def test_is_prime_number():
-    assert is_prime_number(2) is True
-    assert is_prime_number(3) is True
-    assert is_prime_number(5) is True
-    assert is_prime_number(7) is True
-    assert is_prime_number(11) is True
-    assert is_prime_number(13) is True
-    assert is_prime_number(17) is True
-    assert is_prime_number(19) is True
-    assert is_prime_number(23) is True
-    assert is_prime_number(29) is True
-    assert is_prime_number(31) is True
-    assert is_prime_number(37) is True
-    assert is_prime_number(41) is True
-    assert is_prime_number(43) is True
-    assert is_prime_number(47) is True
-    assert is_prime_number(53) is True
-    assert is_prime_number(59) is True
-    assert is_prime_number(61) is True
-    assert is_prime_number(67) is True
-    assert is_prime_number(4) is False
-    assert is_prime_number(44) is False
+def generate_prime_numbers(n):
+    count = 0
+    num = 2
+
+    while count < n:
+        if is_prime_numberV2(num):
+            yield num
+            count += 1
+        num += 1
 
 
-def test_is_prime_number_benchmark(benchmark):
-    # benchmark something
-    result = benchmark.pedantic(is_prime_number, args=(67,), kwargs={}, iterations=10, rounds=100)
+def generate_non_prime_numbers(n):
+    count = 0
+    num = 0
 
-    # Extra code, to verify that the run completed correctly.
-    # Sometimes you may want to check the result, fast functions
-    # are no good if they return incorrect results :-)
-    assert result is True
+    while count < n:
+        if not is_prime_numberV2(num):
+            yield num
+            count += 1
+        num += 1
+
+
+# primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
+primes = list(generate_prime_numbers(10000))
+non_primes = list(generate_non_prime_numbers(10000))
+
+
+def test_is_prime_number_v1():
+    for num in primes:
+        assert is_prime_numberV1(num) is True
+
+    for num in non_primes:
+        assert is_prime_numberV1(num) is False
+
+
+def test_is_prime_number_v2():
+    for num in primes:
+        assert is_prime_numberV2(num) is True
+
+    for num in non_primes:
+        assert is_prime_numberV2(num) is False
+
+
+def test_is_prime_number_benchmark1(benchmark):
+    benchmark(test_is_prime_number_v1)
+
+
+def test_is_prime_number_benchmark2(benchmark):
+    benchmark(test_is_prime_number_v2)
